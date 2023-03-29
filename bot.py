b26cfa4d9794f45992a0f26e8a66230a1eaf0dc8
Paul Busch <paul.busch@planpro.info>
2023-03-29 05:36:57 +0200

    formatted the output

--- bot.py
+++ bot.py
@@ -14,37 +14,26 @@ logging.basicConfig(
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-template = """
-Du bist der Assisten des Meister einer DnD 5e Kampagne. Deine Aufgabe ist es, neue NPCs zu erstellen. 
-Bei den Namen orientiere dich bitte an gängigen DnD-Namen, benutze nicht den Beruf als Namen. 
-Für einen NPC sollst du folgende Informationen generieren:
-- Name
-- Rasse
-- Beruf
-- Gesinnung
-- Äußerliche Erscheinung (maximal 3 Sätze)
-- Charakterliche Eigenschaften (maximal 4 Sätze)
-
-{{user_input}}
-"""
 
 async def npc(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_input = ' '.join(context.args)
-    logging.log(logging.DEBUG, user_input)
-    prompt = template.replace("{{user_input}}", user_input)
-    response = await openai.Completion.acreate(
-        engine="text-davinci-003",
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=350,
-        n=1,
-        stop=None,
-        timeout=10,
-    )
+    with open("prompt_templates/npc.txt", "r") as file:
+        template = file.read()
+        user_input = ' '.join(context.args)
+        logging.log(logging.DEBUG, user_input)
+        prompt = template.replace("{{user_input}}", user_input)
+        response = await openai.Completion.acreate(
+            engine="text-davinci-003",
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=1500,
+            n=1,
+            stop=None,
+            timeout=10,
+        )
 
-    new_npc = response.choices[0].text.strip()
-    logging.log(logging.DEBUG, new_npc)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=new_npc)
+        new_npc = response.choices[0].text.strip().replace(".", "\\.").replace("-", "\\-")
+        logging.log(logging.DEBUG, new_npc)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=new_npc, parse_mode="MarkdownV2")
 
 
 if __name__ == '__main__':
